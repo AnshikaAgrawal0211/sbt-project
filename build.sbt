481b5eb99144e869b01ac31d6eb808fe665b3244
pankajchaudhary5 <pankajchaudhary172@gmail.com>
2019-08-02 11:31:22 +0530

    first step

---
+++ build.sbt
@@ -0,0 +1,8 @@
+name := "sbt-project"
+ version := "0.1"
+scalaVersion := "2.13.0"
+libraryDependencies ++= Seq("com.typesafe.akka" %% "akka-actor" % "2.5.23")
+
+
+
+
---
+++ src/main/scala/Actor.scala
@@ -0,0 +1,17 @@
+import akka.actor.{Actor, ActorSystem, Props}
+
+class Hello extends Actor{
+def receive={
+case msg: String => println(msg)
+case _ => println("unknown message")
+}
+}
+
+object Main{
+def main(args:Array[String]){
+val actorSystem=ActorSystem("ActorSystem")
+val actor = actorSystem.actorOf(Props[Hello],"Hello")
+actor ! "Hello"
+actor ! 100
+}
+}
